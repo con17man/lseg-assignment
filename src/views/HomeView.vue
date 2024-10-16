a12f74cbb40e93c1e36b2bc8c926ae5a831da1a6
Constantin Radu <radu17constantin@gmail.com>
2024-10-16 10:40:05 +0300

    remove unnecessary boilerplate code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- TODO -- add CTA chatbot here -->
   </main>
 </template>
