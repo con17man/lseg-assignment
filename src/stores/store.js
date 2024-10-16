c46b63603dd64ba2071f7f380b40a8591f588cb1
Constantin Radu <radu17constantin@gmail.com>
2024-10-16 11:24:20 +0300

    create store action wrapper to fetch & update state objects

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import fetcher from '@/helpers/fetcher';
 
 const DATA_STORE_OBJ = {
   error: null,
@@ -9,11 +10,44 @@ const DATA_STORE_OBJ = {
 export const useStore = defineStore('store', {
   state: () => ({
     stocks: { ...DATA_STORE_OBJ },
+    error: null,
   }),
 
   getters: {},
 
   actions: {
+    async getData({ mutationKey, url, props, options }) {
+      this[mutationKey] = {
+        isLoading: true,
+        data: null,
+        error: null,
+        ...props,
+      };
+
+      const data = await fetcher(url, options).catch(
+        error => (this.error = error),
+      );
+
+      const payload = {
+        isLoading: false,
+        data: null,
+        error: null,
+        ...props,
+      };
+
+      if (data) {
+        if (!data.error) {
+          payload.data = data;
+        } else {
+          payload.error = data.error;
+        }
+      }
+
+      this[mutationKey] = payload;
+
+      return payload;
+    },
+
     async fetchTopStocks() {
       // TODO -- fetch dummy data
     },
