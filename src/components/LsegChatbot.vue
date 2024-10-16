4395dfd3bd1e33ca4d90c78f9a7276d9b90843dc
Constantin Radu <radu17constantin@gmail.com>
2024-10-17 02:14:18 +0300

    move messages to the bottom; add mock user input

--- src/components/LsegChatbot.vue
+++ src/components/LsegChatbot.vue
@@ -30,14 +30,43 @@ const toggleConversationView = () => {
 
   <!-- Chat window -->
   <div v-if="data.chatToggle" class="chat__wrapper">
-    <!-- for loop to render messages -->
-    <lseg-chat-msg text="Select stock exchange:" :isUser="false"></lseg-chat-msg>
-    <lseg-chat-msg text="LSEG" :isUser="true"></lseg-chat-msg>
+    <div class="chat__main">
+      <!-- for loop to render messages -->
+      <lseg-chat-msg text="Select stock exchange:" :isUser="false"></lseg-chat-msg>
+      <lseg-chat-msg text="LSEG" :isUser="true"></lseg-chat-msg>
+    </div>
+
+    <div class="chat__prompt">
+      <input type="text" name="input" class="chat__input" placeholder="Type a message" disabled>
+      <button class="chat__submit" disabled>
+        <i class="fa-regular fa-paper-plane"></i>
+      </button>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .chat__wrapper {
-  @apply absolute bottom-8 right-24 border-2 border-blue rounded-2xl p-2 w-2/3 max-w-[600px] h-[600px] overflow-y-auto overscroll-contain;
+  @apply absolute flex flex-col justify-end bottom-8 right-24 border-2 border-blue rounded-2xl w-2/3 max-w-[600px] h-[600px] overflow-y-auto overscroll-contain;
+}
+
+.chat__main {
+  @apply relative p-2;
+}
+
+.chat__prompt {
+  @apply bg-gray-200 px-2 py-4 flex justify-between;
+}
+
+.chat__input {
+  @apply w-full bg-transparent mx-2 text-blue outline-none;
+}
+
+.chat__submit {
+  @apply w-6 h-6 mx-2;
+
+  &:disabled {
+    opacity: 0.5;
+  }
 }
 </style>
